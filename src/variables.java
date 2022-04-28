e01e52bf71421c8a521a7c3889a3bdbd419cb096
Betty-Oforiwa <maameakuaoforiwa@gmail.com>
2022-04-28 12:31:18 +0000

    java basics:helloworld,variables-and-data_types

---
+++ src/variables.java
@@ -0,0 +1,28 @@
+package src;
+
+public class variables {
+    public static void main (String[] args){
+//        integer
+        int x;
+        int y;
+
+        x = 4;
+        y = 3;
+        System.out.println(x + y);
+
+//      floating numbers
+        float a ;
+        float b ;
+        a = 4.5f;
+        b = 54.5f;
+        System.out.println(a + b);
+
+//        int and float
+        int iNumber = 5;
+        float fNumber= 5.5f;
+        System.out.println(iNumber);
+        System.out.println(fNumber);
+
+
+    }
+}
